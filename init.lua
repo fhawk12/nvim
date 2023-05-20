798c6406a6b91c017032cfa901b18acdfa6a60d8
fhawk12 <2369187058@qq.com>
2023-05-20 17:02:47 +0800

    update

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+local qt_include_path = "/usr/include/qt6/"
+vim.api.nvim_command('set path+="' .. qt_include_path .. '"')
