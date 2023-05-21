aed7bf74f72c35ddfd6ac83940a6f4ef7a1584db
fhawk12 <2369187058@qq.com>
2023-05-21 11:15:08 +0800

    update
    add competitest plugin

--- init.lua
+++ init.lua
@@ -1,5 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
-local qt_include_path = "/usr/include/qt6/"
-vim.api.nvim_command('set path+="' .. qt_include_path .. '"')
