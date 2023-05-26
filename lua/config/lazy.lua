3a24ecf47934d08e97eeffed81d5e9a2ebb659ec
fhawk12 <2369187058@qq.com>
2023-05-27 05:51:32 +0800

    update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,7 +16,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
-    { import = "lazyvim.plugins.extras.ui.mini-starter" },
     { import = "lazyvim.plugins.extras.lang.json" },
     -- import/override with your plugins
     { import = "plugins" },
